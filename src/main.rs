10b951663a9b7e18509467ae592268c9c9b88e85
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2025-10-02 18:09:13 +0200

    chore: Add TODO

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,8 @@ fn main() {
     // TODO when updating, dependencies look like fj3k29fj, not like fabric-api
     // TODO add launch subcommand to launch a branch
     // TODO move more code to the library
+    // TODO check if version overrides, inclusions and exclusions also work for auto-added
+    // dependencies
     Cli::parse().run();
 }
 
